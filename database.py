90e1eb57f4f96f8ff41b0fbe9c6bd3a4a5b9d66b
Sahana B Raj <sahanabraj234@gmail.com>
2025-05-24 14:43:09 +0530

    Create database.py

---
+++ database.py
@@ -0,0 +1,10 @@
+import sqlite3
+
+conn = sqlite3.connect('data.db')
+c = conn.cursor()
+c.execute('''CREATE TABLE IF NOT EXISTS users (name TEXT, email TEXT)''')
+conn.commit()
+conn.close()
+
+print("Database and table created successfully!")
+
